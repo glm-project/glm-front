f508fba43277e1a64045ea5aad4c3b91802eb3ba
loic <loicjurado@free.fr>
2026-08-03 10:43:24 +0200

    Apply module: angular-core

---
+++ src/main/webapp/app/app.route.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  // seed4j-needle-angular-route
+];
---
+++ src/main/webapp/app/app.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
+import { App } from './app';
+
+describe('App Component', () => {
+  let comp: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideRouter([]), { provide: ComponentFixtureAutoDetect, useValue: true }],
+    }).compileComponents();
+  });
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    comp = fixture.componentInstance;
+  });
+
+  describe('ngOnInit', () => {
+    it('should have appName', () => {
+      expect(comp.appName()).toBe('glmfront');
+    });
+  });
+});
