4725b13812140b577a7f71517ad9dd4050c12d39
Nicolas David <nico01.david@gmail.com>
2026-09-13 23:22:02 +0200

    refactor(gestion): retirer les elements d'amorce du gabarit principal

--- src/main/webapp/gestion/auth.provider.cypress.ts
+++ src/main/webapp/gestion/auth.provider.cypress.ts
@@ -1,5 +1,10 @@
 import { AuthenticationPort } from '@/app/shared/authentication/domain/AuthenticationPort';
 import { InMemoryAuthentication } from '@/app/shared/authentication/infrastructure/secondary/in-memory/InMemoryAuthentication';
+import { ErrorHandlerPort } from '@/app/shared/error-handler/domain/ErrorHandlerPort';
+import { ConsoleErrorHandler } from '@/app/shared/error-handler/infrastructure/secondary/ConsoleErrorHandler';
 import { Provider } from '@angular/core';
 
-export const authProvider: Provider[] = [{ provide: AuthenticationPort, useClass: InMemoryAuthentication }];
+export const authProvider: Provider[] = [
+  { provide: AuthenticationPort, useClass: InMemoryAuthentication },
+  { provide: ErrorHandlerPort, useClass: ConsoleErrorHandler },
+];
